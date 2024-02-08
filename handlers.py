34e4feb63c2c93789db7e61658a40cd453b582ff
Vasiliy Karpyuk <v.a.karpyuk@gmail.com>
2024-02-08 18:56:11 +0300

    Добавил кнопочки

--- handlers.py
+++ handlers.py
@@ -2,15 +2,32 @@ from aiogram import types, F, Router
 from aiogram.types import Message
 from aiogram.filters import Command
 
+import keyBoards
+from keyBoards import mainMenu
+
 
 router = Router()
 
 
 @router.message(Command("start"))
-async def start_handler(msg: Message):
-    await msg.answer("Привет! Я помогу тебе узнать твой ID, просто отправь мне любое сообщение")
+async def start_handler(message: Message):
+    await message.answer("Привет, путник, это Помошник Нейроконсультант", reply_markup=keyBoards.mainMenu)
 
 
-@router.message()
-async def message_handler(msg: Message):
-    await msg.answer(f"Твой ID: {msg.from_user.id}")
+@router.message(Command("neuroZakupki_bot", prefix="@"))
+async def cmd_custom1(message: Message):
+    await message.reply("Вижу команду!")
+
+@router.message(Command("profile"))
+async def cmd_profile(message: types.Message):
+    await message.reply("Профиль")
+
+@router.message(F.text == "Меню")
+async def menu(message: Message):
+    await message.answer('Привет, вот меню', reply_markup=keyBoards.mainMenu)
+
+
+# Пиздеть на всех
+# @router.message()
+# async def message_handler(message: Message):
+#     await message.reply(f"Твой ID: {message.from_user.id}")
--- misc.py
+++ misc.py
@@ -3,6 +3,6 @@ import env.config
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
-
-bot = Bot(token=env.config.BOT_TOKEN, parse_mode=ParseMode.HTML)
-dp = Dispatcher(storage=MemoryStorage())
+#
+# bot = Bot(token=env.config.BOT_TOKEN, parse_mode=ParseMode.HTML)
+# dp = Dispatcher(storage=MemoryStorage())
