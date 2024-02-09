5eb393f0aa2d3ec1283700764de192cf8ce93e05
Vasiliy Karpyuk <v.a.karpyuk@gmail.com>
2024-02-09 12:37:09 +0300

    Получаем статус юзера

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+import logging
+
 import mysql.connector
 from mysql.connector import Error
 import env.config
@@ -12,9 +14,8 @@ def mydbConnection():
             passwd=env.config.db_pass,
             database=env.config.db_name
         )
-        print("Connection to MySQL DB successful")
     except Error as e:
-        print(f"The error '{e}' occurred")
+        logging.error(f"The error '{e}' occurred")
 
     return connection
 
@@ -24,12 +25,30 @@ def mydbConnection():
 def add_new_user(user_id, user_name):
     connection = mydbConnection()
     cursor = connection.cursor()
-    sql = "INSERT INTO users (user_id, user_name) VALUES (%s, %s)"
-    val = (user_id, user_name)
+    cursor.execute(f"SELECT * from users WHERE user_id='{user_id}'")
+    if cursor.fetchall():  # checking if something found with this username
+        logging.info('User_id:' + str(user_id) + ' already exists. His user_name is:' + user_name)
+    else:
+        sql = "INSERT INTO users (user_id, user_name) VALUES (%s, %s)"
+        val = (user_id, user_name)
+        cursor.execute(sql, val)
+        connection.commit()
+        logging.info('New user has been registred, iser_id:' + str(user_id) + ' User_name:' + user_name)
 
-    cursor.execute(sql, val)
-    connection.commit()
-    print("Появился новый пользователь " + user_name)
+
+def check_user_status(user_id, user_name=''):
+    connection = mydbConnection()
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT user_id, status from users WHERE user_id='{user_id}'")
+    myresult = cursor.fetchall()
+    # print(myresult)
+    return myresult[0][1]
 
 
+    # if myresult=cursor.fetchall():  # checking if something found with this username
+    #     logging.info('User_id:' + str(user_id) + ' already exists. His user_name is:' + user_name)
+    # else:
+    #     logging.error('Error while shecking status, iser_id:' + str(user_id) + ' User_name:' + user_name)
 
+myresult1 = check_user_status(243697626, 'Василий')
+print(myresult1)
