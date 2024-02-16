f37903a5c2190fd5f256c00534b7056c219faa6d
Vasiliy Karpyuk <v.a.karpyuk@gmail.com>
2024-02-16 12:32:07 +0300

    Бот отвечает, но пока не редактирует

--- main.py
+++ main.py
@@ -14,29 +14,27 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 telethon_api_id = getenv('telethon_api_id')
 telethon_api_hash = getenv('telethon_api_hash')
-from handlers import router, start_go_test_handler, i_see_edits_handler, i_see_response_handler
+from handlers import router, i_see_edits_handler, i_see_response_handler, handler_test
 from telethon import TelegramClient  #, sync, events
 
-async def main():
+async def main_aiogram_bot():
     bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
-async  def proxi_to_china():
-    client = TelegramClient('.venv/session_name', telethon_api_id, telethon_api_hash)
-    client.add_event_handler(start_go_test_handler)
-    client.add_event_handler(i_see_response_handler)
-    client.add_event_handler(i_see_edits_handler)
-
-    await client.start()
-
+async def proxi_to_china():
+    client_proxy_to_telegram = TelegramClient('.venv/send_msg_to_china', telethon_api_id, telethon_api_hash)
+    client_proxy_to_telegram.add_event_handler(i_see_response_handler)
+    await  client_proxy_to_telegram.start()
     try:
-        # await plugins.init(client) # хз что это
-        await client.run_until_disconnected()
+        await client_proxy_to_telegram.run_until_disconnected()
     finally:
-        await client.disconnect()
+        await client_proxy_to_telegram.disconnect()
+
+async def main():
+    await asyncio.gather(main_aiogram_bot(), proxi_to_china())   #
 
 
 if __name__ == "__main__":
@@ -45,4 +43,3 @@ if __name__ == "__main__":
         asyncio.run(main())
     elif server_mode=="TEST":
         asyncio.run(main())
-        asyncio.run(proxi_to_china())
