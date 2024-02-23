5545dd2934aacda6aac629820122eab952a23219
Vasiliy Karpyuk <v.a.karpyuk@gmail.com>
2024-02-23 13:22:19 +0300

    Вроде из этого положения запускались обе ручки i_see_edits_handler, i_see_response_handler но сча не

--- main.py
+++ main.py
@@ -19,13 +19,6 @@ env_telethon_api_id = getenv('env_telethon_api_id')
 env_telethon_api_hash = getenv('env_telethon_api_hash')
 env_telethon_session = ".venv/session_name.session"
 
-env_db_host = getenv('env_db_host')
-env_db_username = getenv('env_db_username')
-env_db_pass = getenv('env_db_pass')
-env_db_name = getenv('env_db_name')
-
-# cache_dict = dict()
-# cache_dict = {'dict': 1, 'dictionary': 2}
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
