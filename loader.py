3bb178eaa5e4d6bafd0beee0f44b5c61dfa776df
Vasiliy Karpyuk <v.a.karpyuk@gmail.com>
2024-02-25 22:13:10 +0300

    Асинхронные изменения сообщений подъехали

--- loader.py
+++ loader.py
@@ -6,15 +6,53 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from dotenv import load_dotenv
 from telethon import TelegramClient
 
-load_dotenv('.venv/.env')
+from MySQLStorage import MySQLStorage
+from main_bot_iaogram.handlers_aiogram import router
+from proxy_telethron.handlers_telethon import i_see_response_handler, i_see_edits_handler
 
+load_dotenv('.venv/.env')
+env_telethon_session = getenv('env_telethon_session')
 env_telethon_api_id = getenv('env_telethon_api_id')
 env_telethon_api_hash = getenv('env_telethon_api_hash')
 env_main_tg_bot_token = getenv('env_main_tg_bot_token')
+env_server_mode = getenv('env_server_mode')
+env_db_name = getenv('env_db_name')
+env_db_username = getenv('env_db_username')
+env_db_pass = getenv('env_db_pass')
+env_db_host = getenv('env_db_host')
 
-client_telethron = TelegramClient(session="session_name2.session",
-                                      api_id=int(env_telethon_api_id),
-                                      api_hash=env_telethon_api_hash)
+# В этом глобальном словаре хранятся айдишники сообщений, чтобы бот мог к правильному сообщению отвечать
+memory_msgs_ids_dict = dict()
 
-bot = Bot(token=env_main_tg_bot_token, parse_mode=ParseMode.HTML)
-dp = Dispatcher(storage=MemoryStorage())
+# Создаем бота и диспетчера Иаограмм, чтобы потом ими пользоваться везде
+bot = Bot(token=env_main_tg_bot_token,
+          parse_mode=ParseMode.HTML)
+
+# Для локального запуска без базы данных
+if env_server_mode == "PROD":
+    aiogram_storage = MySQLStorage(host=env_db_host,
+                                   user=env_db_username,
+                                   password=env_db_pass,
+                                   database=env_db_name)
+elif env_server_mode == "TEST":
+    aiogram_storage = MemoryStorage()
+dp = Dispatcher(storage=aiogram_storage)
+
+# Создаем клиента telethron, чтобы потом ими пользоваться везде
+client_telethron = TelegramClient(session=env_telethon_session,
+                                  api_id=int(env_telethon_api_id),
+                                  api_hash=env_telethon_api_hash)
+
+async def main_aiogram_bot():
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+async def main_telethron_bot():
+    client_telethron.add_event_handler(i_see_response_handler)
+    client_telethron.add_event_handler(i_see_edits_handler)
+    await client_telethron.start()
+    try:
+        await client_telethron.run_until_disconnected()
+    finally:
+        await client_telethron.disconnect()
--- proxy_telethron/handlers_telethon.py
+++ proxy_telethron/handlers_telethon.py
@@ -3,64 +3,57 @@ from os import getenv
 from pprint import pprint
 
 from dotenv import load_dotenv
-from telethon import TelegramClient
-
+from telethon import events
 import loader as ld
+from local_cache import memory_dict_add_new_cashe
 
 load_dotenv('.venv/.env')
-
-from telethon import events
-
 env_chat_for_exchenge_with_coze_bot = -1002023371936
 env_telethon_api_id = getenv('env_telethon_api_id')
 env_telethon_api_hash = getenv('env_telethon_api_hash')
 env_telethon_session = getenv('env_telethon_session')
-env_coze_bot_id = getenv('env_coze_bot_id')
-env_coze_bot_id_id = int(getenv('env_coze_bot_id_id'))
+env_3th_party_bot = getenv('env_3th_party_bot')
+env_3th_party_bot_id = int(getenv('env_3th_party_bot_id'))
 
+# Пересылаем сообщение от пользователя в телеграмме в группу со сторонним ботом
+async def send_msg_to_3th_party_bot_via_tg(message, user_chat_id, user_message_id):
+    logging.info(f"Отправляем сообщение в Telethron. send_msg_to_3th_party_bot_via_tg{message}")
+    message = "@" + env_3th_party_bot + " " + message
+    proxy_message = await ld.client_telethron.send_message(entity=env_chat_for_exchenge_with_coze_bot,
+                                           message=message)
+    proxy_message_id = proxy_message.to_dict()["id"]
+    memory_dict_add_new_cashe(user_chat_id, user_message_id, proxy_message_id)
 
-
-
-# TODO:Вот этот запрос бы прикрутить к готовеньким TelegramClient
-async def send_msg_to_coze_bot_via_tg(message, user_chat_id, user_message_id):
-    logging.info(f"Отправляем сообщение в Telethron. send_msg_to_coze_bot_via_tg{message}")
-    await ld.client_telethron.start()
-    message = "@" + env_coze_bot_id + " " + message
-    await ld.client_telethron.send_message(entity=env_chat_for_exchenge_with_coze_bot,
-                                        message=message)
-
-
-
-# ________________ Стандартные Ручки тг клиента
-
-# Тестовая ручка, чтобы наскоряк что-то проверить
-@events.register(events.NewMessage(chats="v_karpyuk"))  # chats=('v_karpyuk')
-async def start_go_test_handler(event):
-    message = str(event.message.to_dict()['message'])
-    if message.startswith("Го") or message.startswith("Uj"):
-        logging.info("Увидели сообщение с текстом 'Го' или 'Uj' ")
-        pass
-
+# ________________ Стандартные Ручки Telethron клиента
 
 @events.register(events.NewMessage())   #from_users=env_coze_bot_id, chats=env_chat_for_exchenge_with_coze_bot
 async def i_see_response_handler(event):
 
-
     message_to_dict = event.message.to_dict()
-    # pprint(message_to_dict)
+    print(message_to_dict)
     user_id = message_to_dict["from_id"]["user_id"]
-    # print(user_id)
-    if user_id == env_coze_bot_id_id:
 
-        print("i_see_response_handler: " + message_to_dict["message"])
-        from main_bot_iaogram.handlers_aiogram import \
-            send_response_from_bot_to_user
+    if user_id == env_3th_party_bot_id:
+        reply_to_msg_id = message_to_dict["reply_to"]["reply_to_msg_id"]
+        message = message_to_dict["message"]
+        print("i_see_response_handler: " + message)
+        from main_bot_iaogram.handlers_aiogram import send_response_from_bot_to_user
 
-        await send_response_from_bot_to_user(user_chat_id=env_chat_for_exchenge_with_coze_bot,
-                                             message_text=message_to_dict["message"])
+        await send_response_from_bot_to_user(reply_to_msg_id=reply_to_msg_id,
+                                             message_text=message)
 
 
 @events.register(events.MessageEdited())
-async def i_see_edits_handler(event):                     #     TODO: Добавить send_edits_from_bot_to_user или типа того
-    arr = event.message.to_dict()
-    print("i_see_edits_handler: " + arr["message"])
+async def i_see_edits_handler(event):
+
+    message_to_dict = event.message.to_dict()
+    user_id = message_to_dict["from_id"]["user_id"]
+    print(message_to_dict)
+
+    if user_id == env_3th_party_bot_id:
+        reply_to_msg_id = message_to_dict["reply_to"]["reply_to_msg_id"]
+        message = message_to_dict["message"]
+        print("i_see_edits_handler: " + str(reply_to_msg_id))
+        from main_bot_iaogram.handlers_aiogram import edit_response_from_bot_to_user
+        await edit_response_from_bot_to_user(replay_massage_id=reply_to_msg_id,
+                                             message_text=message)
